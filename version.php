b8a442a912a80fd972022a4cfd06222887e32edb
Benedikt Schneider <benedikt.schneider@hrz.tu-darmstadt.de>
2019-07-16 16:54:03 +0200

    Update version

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018072100;
+$plugin->version = 2019071601;
 $plugin->requires = 2013110500;
 $plugin->component = 'assignfeedback_author';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.5';
+$plugin->release = 'v3.7';
 
 $plugin->dependencies = array(
     'assignsubmission_author' => 2018072100
